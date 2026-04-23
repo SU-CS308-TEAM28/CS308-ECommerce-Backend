6d8c453f96c8329d9cc40476351084573c6d1bac
KRXDevelops-Kaan <kaanuyumaz.developer@gmail.com>
2026-04-23 14:07:25 +0300

    Test endpoint updated to conform to the newest structure.

--- src/main/java/edu/sabanciuniv/cs308ecommercebackend/controllers/TestController.java
+++ src/main/java/edu/sabanciuniv/cs308ecommercebackend/controllers/TestController.java
@@ -1,28 +1,24 @@
 package edu.sabanciuniv.cs308ecommercebackend.controllers;
 
-import edu.sabanciuniv.cs308ecommercebackend.repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
+import edu.sabanciuniv.cs308ecommercebackend.models.payloads.TeknocsResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-public class TestController {
-
-    @Autowired
-    public ProductRepository productRepository;
+public class TestController
+{
 
     @GetMapping("/test")
-    public ResponseEntity<Map<String, Object>> test () {
-        return ResponseEntity.ok(
-                Map.of(
-                        "message", "Test endpoint success.",
-                        "currentProductCount", productRepository.findAll(Pageable.ofSize(50)).stream().count() ,
-                        "timestamp", String.valueOf(System.currentTimeMillis())
-                )
+    public TeknocsResponse<Map<String, Object>> test ()
+    {
+        return new TeknocsResponse<>(
+                HttpStatus.OK,
+                "Test endpoint request successful.",
+                Map.of("timestamp", String.valueOf(System.currentTimeMillis()))
         );
     }
+
 }
