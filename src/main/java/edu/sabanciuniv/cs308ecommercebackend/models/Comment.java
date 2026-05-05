405e7d5a8ee2eed99aa43adb1b535cf5f8da2f06
KRXDevelops-Kaan <kaanuyumaz.developer@gmail.com>
2026-05-05 14:55:22 +0300

    Get Comments and Add Comment endpoints are implemented.

---
+++ src/main/java/edu/sabanciuniv/cs308ecommercebackend/models/Comment.java
@@ -0,0 +1,37 @@
+package edu.sabanciuniv.cs308ecommercebackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "Comments")
+public class Comment
+{
+    @Id
+    String id;
+    String productId;
+    Commenter commenter;
+    Date creationDate;
+    int rate;
+    String comment;
+    boolean isApproved;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Commenter
+    {
+        String id;
+        String publicName;
+    }
+}
